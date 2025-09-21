f0bc01f9a039b4b328350ffe34e63e6dcc00ea0b
Nelson <nelsonr9608@nauta.cu>
2025-09-21 18:02:01 -0400

    Sincronización automática

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 mkdir server
 python3 -m http.server -d server &
-python3 main.py init geminiimg@chat.lylapol.com img123456789
+python3 main.py import geminibot.tar
 python3 main.py link
 python3 main.py serve
