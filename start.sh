c8b352e8221ed36d81525233a72918b78d6985f0
Nelson <nelsonr9608@nauta.cu>
2025-09-21 19:00:46 -0400

    Sincronización automática

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@ mkdir server
 python3 -m http.server -d server &
 python3 main.py import geminibot.tar
 python3 main.py link
+python3 main.py config skip_start_messages 1
+python3 main.py config displayname GemImage
 python3 main.py serve
