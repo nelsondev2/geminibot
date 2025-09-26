e2f735a14d422c43a28dc5a87de7c7f48a3e6aee
Nelson <nelsonr9608@nauta.cu>
2025-09-26 18:33:14 -0400

    correccion displayname

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ python3 -m http.server -d server &
 python3 main.py import geminibot.tar
 python3 main.py link
 python3 main.py config skip_start_messages 1
-python3 main.py config displayname GemImage
+python3 main.py config displayname GemImg
 python3 main.py config selfavatar covers.png
 python3 main.py serve
